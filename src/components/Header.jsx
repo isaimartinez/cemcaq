1ebbfee887289ee3b98522fd7a3a44b8512c6113
Isai Martinez <isai.martinez@yeico.com>
2023-02-20 22:32:28 -0600

    Logo navigate to /

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Logo from '../assets/logo.png'
 
+import {useNavigate} from 'react-router-dom'
+
 const Header = ({title}) => {
+  const navigate = useNavigate()
+
+
+
+  const goToHome = () => {
+    navigate(`/`)
+  }
   return (
     <div className='flex flex-row justify-between p-1 h-24 m-5 bg-secondary-dark rounded'>
-      <div className='flex basis-1/3'>
+      <div className='flex basis-1/3 cursor-pointer' onClick={goToHome}>
         <img src={Logo} alt="Logo" className='bg-white rounded m-2 '/>
       </div>
       <div className='flex basis-1/3 items-center justify-center '>
